9f8594e8a87876665f91342e513d694653ae519a
Daisy-bbfish <daisy.bbfish@gmail.com>
2017-11-29 10:32:17 +0800

    update comment

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-# 1. Downloading and unzipping dataset
+# Step 1. Downloading and unzipping dataset
 if(!file.exists("./data")){dir.create("./data")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl,destfile="./data/Dataset.zip")
@@ -6,40 +6,40 @@ download.file(fileUrl,destfile="./data/Dataset.zip")
 # Unzip dataSet to /data directory
 unzip(zipfile="./data/Dataset.zip",exdir="./data")
 
-# 2. Merging the training and the test sets to create one data set:
+# Step 2. Merging the training and the test sets to create one data set:
 # Reading trainings tables:
-x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
-y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
-subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
+x_train_data <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+y_train_data <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
+subject_train_data <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
 
 # Reading testing tables:
-x_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
-y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
-subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
+x_test_data <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+y_test_data <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
+subject_test_data <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
 
 # Reading feature vector:
-features <- read.table('./data/UCI HAR Dataset/features.txt')
+features_data <- read.table('./data/UCI HAR Dataset/features.txt')
 
 # Reading activity labels:
-activityLabels = read.table('./data/UCI HAR Dataset/activity_labels.txt')
+activityLabels_data = read.table('./data/UCI HAR Dataset/activity_labels.txt')
 
 # Assigning column names:
-colnames(x_train) <- features[,2] 
-colnames(y_train) <-"activityId"
-colnames(subject_train) <- "subjectId"
+colnames(x_train_data) <- features[,2] 
+colnames(y_train_data) <-"activityId"
+colnames(subject_train_data) <- "subjectId"
 
-colnames(x_test) <- features[,2] 
-colnames(y_test) <- "activityId"
-colnames(subject_test) <- "subjectId"
+colnames(x_test_data) <- features_data[,2] 
+colnames(y_test_data) <- "activityId"
+colnames(subject_test_data) <- "subjectId"
 
-colnames(activityLabels) <- c('activityId','activityType')
+colnames(activityLabels_data) <- c('activityId','activityType')
 
 # Merging all data in one set:
-mrg_train <- cbind(y_train, subject_train, x_train)
-mrg_test <- cbind(y_test, subject_test, x_test)
-setAllInOne <- rbind(mrg_train, mrg_test)
+mrg_train_data <- cbind(y_train_data, subject_train_data, x_train_data)
+mrg_test_data <- cbind(y_test_data, subject_test_data, x_test_data)
+setAllInOne <- rbind(mrg_train_data, mrg_test_data)
 
-# 3. Extracting only the measurements on the mean and standard deviation for each measurement
+# Step 3. Extracting only the measurements on the mean and standard deviation for each measurement
 # Reading column names
 colNames <- colnames(setAllInOne)
 
@@ -52,16 +52,15 @@ mean_and_std <- (grepl("activityId" , colNames) |
 # Making nessesary subset from setAllInOne
 setForMeanAndStd <- setAllInOne[ , mean_and_std == TRUE]
 
-# 4.Using descriptive activity names to name the activities in the data set:
+# Step 4.Using descriptive activity names to name the activities in the data set:
 setWithActivityNames <- merge(setForMeanAndStd, activityLabels,
                               by='activityId',
                               all.x=TRUE)
 
-# 5. Creating a second, independent tidy data set with the average of each variable for each activity and each subject:
+# Step 5. Creating a second, independent tidy data set with the average of each variable for each activity and each subject:
 # Making second tidy data set
 secTidyDataSet <- aggregate(. ~subjectId + activityId, setWithActivityNames, mean)
 secTidyDataSet <- secTidyDataSet[order(secTidyDataSet$subjectId, secTidyDataSet$activityId),]
 
 # Writing second tidy data set in txt file
 write.table(secTidyDataSet, "secTidyDataSet.txt", row.name=FALSE)
-
